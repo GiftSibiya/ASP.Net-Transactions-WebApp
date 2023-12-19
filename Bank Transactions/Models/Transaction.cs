feefe8945f38e1e9acfb2f260ef56b1301933ce2
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-19 15:53:49 +0200

    Add project files.

---
+++ Bank Transactions/Models/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank_Transactions.Models
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+        public int TransactionID { get; set; }
+        public int TransactionID { get; set; }
+
+    }
+}
